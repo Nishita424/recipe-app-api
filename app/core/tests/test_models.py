65643978bcda02b78b6c94f94ae6cb0dde9b8dd4
nishitagiduturi <nishitaoct26@gmail.com>
2020-10-24 20:15:35 +0530

    Updated admin panel to be able to change and add new user to custom User model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -16,16 +16,20 @@ class ModelTests(TestCase):
     def test_is_new_user_email_normalised(self):
         """Test the email for new user is normalised(lower case)"""
         email = "test@GMAIL.COM"
-        user = get_user_model().objects.create_user(email=email, password="test123")
+        user = get_user_model().objects.create_user(email=email,
+                                                    password="test123")
         self.assertEqual(user.email, email.lower())
 
     def test_if_user_email_is_empty(self):
-        """Test the new user email, and create user if email is not empty otherwise raise ValueError"""
+        """Test the new user email, and create user if email is not empty
+        otherwise raise ValueError"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(email=None, password='test123')
+            get_user_model().objects.create_user(email=None,
+                                                 password='test123')
 
     def test_create_new_super_user(self):
         """Test for new super user"""
-        user = get_user_model().objects.create_super_user(email='test@GMAIL.com', password='password123')
+        user = get_user_model().objects.create_superuser(
+            email='test@GMAIL.com', password='password123')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
