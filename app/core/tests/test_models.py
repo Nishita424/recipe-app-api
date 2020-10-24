b1eff43bf0a9599afffc08e1cdf905b82c7fe92d
nishitagiduturi <nishitaoct26@gmail.com>
2020-10-24 15:05:19 +0530

    * Created new User model that customizes the base User model and UserManager * Normalised user email in custom UserManager

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with email is successful"""
+        email = 'test@gmail.com'
+        password = 'password123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_is_new_user_email_normalised(self):
+        """Test the email for new user is normalised(lower case)"""
+        email = "test@GMAIL.COM"
+        user = get_user_model().objects.create_user(email=email, password="test123")
+        self.assertEqual(user.email, email.lower())
